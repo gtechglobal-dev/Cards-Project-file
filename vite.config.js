7b140f6e52e62e2fd0969198025d450a48aa4a77
gtechglobal-dev <gtechglobal.dev@gmail.com>
2026-03-05 17:19:46 +0100

    Add files via upload
    added cards update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/Cards-Project-file/",
+export default defineConfig(({ mode }) => ({
   plugins: [
     react({
       babel: {
@@ -10,4 +9,6 @@ export default defineConfig({
       },
     }),
   ],
-});
+
+  base: mode === "production" ? "/Cards-Project-file/" : "/",
+}));
